420a1eef20b53169a54592f29dffff731b72e0f1
Praveen-22k <you@example.co git config --global user.email nakesh376@gmail.com>
2025-11-18 16:39:25 +0530

    new commit

---
+++ src/assets/assests.js
@@ -0,0 +1,7 @@
+import Ba1 from './Ba1.jpeg'
+import Ba2 from './Ba2.jpeg'
+import Ba3 from './Ba3.jpeg'
+import Banner1 from './Banner1.jpg'
+export{
+    Ba1,Ba2,Ba3,Banner1
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import Navbar from "../component/Navbar/Navbar";
 import Banner from "../component/Banner/Banner";
+import Content from '../component/Content'
+import Content2 from '../component/Content2'
+import Footer from '../component/Footer/Footer'
 const Home = () => {
   return (
   <div> <Navbar/>
-  
-  
-  
+  <Banner/>
+  <Content/>
+  <Content2/>
+  <Footer/>
   </div>
 )
 };
